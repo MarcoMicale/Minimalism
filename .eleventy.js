b1597c4ebe29e64a5917078352e4416d106d6d38
Marco Micale <13455214+MarcoMicale@users.noreply.github.com>
2022-04-16 15:57:51 +0200

    Social, small in css; Ucraine Flag removed, anonymization & centered titles

--- .eleventy.js
+++ .eleventy.js
@@ -24,7 +24,7 @@ module.exports = function (eleventyConfig) {
 
 	// Copia alcuni file statici
 	eleventyConfig
-		.addPassthroughCopy({ 'src/_11ty/_static/app/*.*': '/' })
+		// .addPassthroughCopy({ 'src/_11ty/_static/app/*.*': '/' })
 		.addPassthroughCopy({ 'src/_11ty/_static/favicon': 'favicon' })
 		.addPassthroughCopy({ 'src/_11ty/_static/img': 'img' })
 
